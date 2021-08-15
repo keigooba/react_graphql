07f4cb88ac24dbabc89e6b37cac7882e623bf6e8
keigooba <keigo2356@icloud.com>
2021-08-15 18:09:31 +0900

    first commit

---
+++ src/components/DeptList.js
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';
+import { StateContext } from "../context/StateContext";
+import styles from "./DeptList.module.css";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { useMutation } from "@apollo/client";
+import { CREATE_DEPT, DELETE_DEPT, GET_DEPTS, GET_EMPLOYEES } from "../queries";
+
+const DeptList = ({ dataDepts }) => {
+  const { deptName, setDeptName } = useContext(StateContext);
+  const [createDept] = useMutation(CREATE_DEPT, {
+    refetchQueries: [{ query: GET_DEPTS }],
+  });
+  const [deleteDept] = useMutation(DELETE_DEPT, {
+    refetchQueries: [{ query: GET_DEPTS }, { query: GET_EMPLOYEES }],
+  });
+  return (
+    <>
+      <h3>Department List</h3>
+      <input
+        className={styles.deptList__input}
+        placeholder="new department name"
+        type="text"
+        value={deptName}
+        onChange={(e) => {
+          setDeptName(e.target.value);
+        }}
+      />
+      <button
+        disabled={!deptName}
+        onClick={async () => {
+          try {
+            await createDept({
+              variables: {
+                deptName: deptName,
+              },
+            });
+          } catch (err) {
+            alert(err.message);
+          }
+          setDeptName("");
+        }}
+      >
+      New dept
+      </button>
+    </>
+  )
+}
+
+export default DeptList
---
+++ src/queries.js
@@ -0,0 +1,133 @@
+import gql from "graphql-tag";
+
+export const CREATE_USER = gql`
+  mutation($username: String!, $password: String!) {
+    createUser(input: {username: $username, password: $password, email: ""}) {
+      user {
+        id
+        username
+      }
+    }
+  }
+`;
+export const GET_TOKEN = gql`
+  mutation($username: String!, $password: String!) {
+    tokenAuth(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+export const GET_EMPLOYEES = gql`
+query {
+  allEmployees {
+    edges {
+      node {
+        id
+        name
+        joinYear
+        department {
+          id
+          deptName
+        }
+      }
+    }
+  }
+}
+`;
+export const DELETE_DEPT = gql`
+mutation ($id: ID!) {
+  deleteDept(input: {id: $id}) {
+    department {
+      id
+    }
+  }
+}
+`;
+export const GET_DEPTS = gql`
+query {
+  allDepartments {
+    edges {
+      node {
+        id
+        deptName
+      }
+    }
+  }
+}
+`;
+export const CREATE_DEPT = gql`
+mutation($deptName: String!) {
+  createDept(input: {deptName: $deptName}) {
+    department {
+      id
+      deptName
+    }
+  }
+}
+`;
+export const CREATE_EMPLOYEE = gql`
+  mutation ($name: String!, $joinYear: Int!, $department: ID!) {
+    createEmployee(
+      input: {name: $name, joinYear: $joinYear, department: $department}
+    ) {
+      employee {
+        id
+        name
+        joinYear
+        department {
+          id
+          deptName
+        }
+      }
+    }
+  }
+`;
+export const UPDATE_EMPLOYEE = gql`
+mutation (
+  $id: ID!,
+  $name: String!,
+  $joinYear: Int!,
+  $department: ID!
+){
+  updateEmployee(
+    input: {
+      id: $id
+      name: $name
+      joinYear: $joinYear
+      department: $department
+    }
+  ) {
+    employee {
+      id
+      name
+      joinYear
+      department {
+        id
+        deptName
+      }
+    }
+  }
+}
+`;
+export const DELETE_EMPLOYEE = gql`
+mutation($id: ID!) {
+  deleteEmployee(input: {id: $id}) {
+    employee {
+      id
+    }
+  }
+}
+`;
+export const GET_SINGLE_EMPLOYEE = gql`
+query($id: ID!) {
+  employee(id: $id) {
+    id
+    name
+    joinYear
+    department {
+      id
+      deptName
+    }
+  }
+}
+`;
